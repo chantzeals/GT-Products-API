ddcad84d5a07ea4a49ce464737781c42f96d9645
chantzeals <randzandrea@gmail.com>
2025-08-30 15:14:43 +0800

    Updated File

--- Helloworld-api/in-memory.js
+++ Helloworld-api/in-memory.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 
 let products = [{"id":1, "name":"Laptop", "price":1000}, 
-                {"id":2, "name": "Smartphone", "price": 500},    
+                {"id":2, "name": "Smartphones", "price": 500},    
                 {"id":3, "name":"Tablet", "price":300}];
 
 
