98ba767e6465241d85b37ed891f9148a28387679
chantzeals <randzandrea@gmail.com>
2025-09-12 20:16:33 +0800

    Challenge 3

--- Helloworld-api/index.js
+++ Helloworld-api/index.js
@@ -1,17 +1,14 @@
 import express from 'express';
 import productRoutes from './src/Routes/post.routes.js';
 import commentRoutes from './src/Routes/comment.route.js';
-import morgan from 'morgan'; 
-import dotenv from 'dotenv';
-
-dotenv.config();
+import morgan from 'morgan';
+import config from './src/config/index.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV === 'development') {
+if (config.nodeEnv === 'development') {
   console.log('Running in development mode');
-} else if (process.env.NODE_ENV === 'production') {
+} else if (config.nodeEnv === 'production') {
   console.log('Running in production mode');
 } else {
   console.log('Running in unknown mode');
@@ -22,6 +19,6 @@ app.use(express.json());
 app.use('/', commentRoutes);
 app.use('/products', productRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(config.port, () => {
+  console.log(`Server is running on http://localhost:${config.port}`);
 });
