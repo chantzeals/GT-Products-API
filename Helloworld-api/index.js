d1f9d00bb7cdeb50e2b4c9d33a80d26a53ac3b14
chantzeals <randzandrea@gmail.com>
2025-09-12 20:03:59 +0800

    add: env and gitignore

--- Helloworld-api/index.js
+++ Helloworld-api/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import productRoutes from './src/Routes/post.routes.js';
 import commentRoutes from './src/Routes/comment.route.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/', commentRoutes);
