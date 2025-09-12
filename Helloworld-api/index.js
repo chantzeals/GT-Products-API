c35ea9b64e920d09ab34cefcc7ca06b4fffdac02
chantzeals <randzandrea@gmail.com>
2025-09-12 20:10:42 +0800

    Challenge 1

--- Helloworld-api/index.js
+++ Helloworld-api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import productRoutes from './src/Routes/post.routes.js';
 import commentRoutes from './src/Routes/comment.route.js';
+import morgan from 'morgan'; 
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,6 +9,16 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+if (process.env.NODE_ENV === 'development') {
+  console.log('Running in development mode');
+} else if (process.env.NODE_ENV === 'production') {
+  console.log('Running in production mode');
+} else {
+  console.log('Running in unknown mode');
+}
+
+
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/', commentRoutes);
 app.use('/products', productRoutes);
